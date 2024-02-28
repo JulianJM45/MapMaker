ec2b77c6936069aaebf83c1698b40b5d94b6bbdc
JulianJM45 <julian@clami.net>
2024-02-28 11:32:33 +0100

    compileExe adapted to windows

--- compileEXE.py
+++ compileEXE.py
@@ -1,4 +1,14 @@
 import subprocess
+import platform
+
+if platform.system() == 'Windows':
+    executable = 'realesrgan-ncnn-vulkan.exe'
+elif platform.system() == 'Linux':
+    executable = 'realesrgan-ncnn-vulkan'
+elif platform.system() == 'Darwin':
+    executable = 'realesrgan-ncnn-vulkan'
+else:
+    print('Unknown OS')
 
 command = [
     "pyinstaller",
@@ -8,7 +18,7 @@ command = [
     "--add-data", "index.html:.",
     "--add-data", "styles.css:.",
     "--add-data", "scripts.js:.",
-    "--add-binary", "modules/realesrgan-ncnn-vulkan:./modules",
+    "--add-binary", f"modules/{executable}/:./modules",
     "--add-data", "modules/models:./modules/models",
     "main.py"
 ]
